b29779f15ac6fe3e3e931faec4d30d43981cc66c
Cristina <cris.glez512@gmail.com>
2025-05-11 20:05:51 +0200

    Organización de las SELECT (parte1.1)

---
+++ bbdd/sentencias/SELECT_Diego.sql
@@ -0,0 +1,11 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red193\green193\blue193;}
+{\*\expandedcolortbl;;\cssrgb\c80000\c80000\c80000;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww33700\viewh18520\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs26 \cf2 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 \
+SELECT ID_USUARIO, NOMBRE, EMAIL, FECHA_REGISTRO FROM USUARIO WHERE FECHA_REGISTRO > TO_DATE(\'912023-07-01\'92, \'91YYYY-MM-DD\'92);}
