56cc91f4a6b97029fbda4bbd3a324b3f61b34cb9
Ángel Romero Guijo <romeroguijoangel6@gmail.com>
2025-05-13 18:59:09 +0200

    Conexión a base de datos
    Conexion a base de datos y creación primer sql de la tabla proyectos.

--- gestionTareas/src/main/kotlin/module-info.java
+++ gestionTareas/src/main/kotlin/module-info.java
@@ -2,6 +2,7 @@ module org.example.gestiontareas {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
+    requires java.sql;
 
 
     opens org.example.gestiontareas to javafx.fxml;
